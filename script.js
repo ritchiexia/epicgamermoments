29b727c19361e69e18aa78de3da5cdabdba823d0
ritchiexia <ritchie.xia@gmail.com>
2021-02-21 03:10:02 -0800

    Visuals are done. Finished for presentation, just need future updates to fully implement.

--- script.js
+++ script.js
@@ -3,3 +3,27 @@
 //       document.body.classList.toggle("dark");
 //     }
 //   }
+var epicClick = 0;
+var cringeClick = 0;
+var pogClick = 0;
+var bruhClick = 0;
+
+function onClick1() {
+    epicClick += 1;
+    document.getElementById("epicClick").innerHTML = epicClick;
+};
+
+function onClick2() {
+    cringeClick += 1;
+    document.getElementById("cringeClick").innerHTML = cringeClick;
+};
+
+function onClick3() {
+    pogClick += 1;
+  document.getElementById("pogClick").innerHTML = pogClick;
+};
+
+function onClick4() {
+    bruhClick += 1;
+  document.getElementById("bruhClick").innerHTML = bruhClick;
+};
