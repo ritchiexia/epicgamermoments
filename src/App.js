e9ead1715621532514c09a234b7b1dccaaf88608
elchen <echen27@students.cs.ubc.ca>
2022-02-21 21:09:43 -0800

    created components folder with Navbar, Home, and Games. Installed React Router and set it up in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./styles.css";
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Games from "./components/Games"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/games" element={<Games/>} />
+      </Routes>
+    </div>
+  </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h2>Epic Gamer Moments</h2>
+            <ul className="nav-links">
+                <li>Home</li>
+                <li>Games</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
