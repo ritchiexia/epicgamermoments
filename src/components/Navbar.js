bc88e6e4dcbbb9d40a3c79f68a6f9cec7a8f0754
elchen <echen27@students.cs.ubc.ca>
2022-02-21 21:58:03 -0800

    added buttons to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,8 @@ const Navbar = () => {
     return (
         <nav>
             <h2>Epic Gamer Moments</h2>
-            <ul className="nav-links">
-                <li>Home</li>
-                <li>Games</li>
-            </ul>
+            <button>Home</button>
+            <button>Games</button>
         </nav>
     )
 }
