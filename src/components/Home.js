44840e617a9dfe787705d669d28f42a3eadbad3c
ritchiexia <ritchie.xia@gmail.com>
2022-02-25 23:08:01 -0800

    Gave setPosts a use

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import Posts from "./Posts";
 import { useState } from "react";
-import pitch from "../videos/pitch.mp4"
-import epicCSPlay from "../videos/eesee's EPIC cs play.mp4"
+import pitch from "../videos/pitch.mp4";
+import epicCSPlay from "../videos/eesee's EPIC cs play.mp4";
 
 const Home = () => {
   const [posts, setPosts] = useState([
@@ -9,13 +9,13 @@ const Home = () => {
       id: 1,
       title: "EPIC GAMER MOMENT WEBSITE PREMIER",
       video: pitch,
-      op: "EGM"
+      op: "EGM",
     },
     {
       id: 2,
       title: "EPIC CS PLAY",
       video: epicCSPlay,
-      op: "eesee"
+      op: "eesee",
     },
   ]);
 
@@ -23,6 +23,7 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       <Posts posts={posts} />
+      {0 === 1 && setPosts(null)}
     </div>
   );
 };
